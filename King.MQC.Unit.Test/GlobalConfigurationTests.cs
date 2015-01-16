5edb3b2140bdb51af533c1a20fe09ef2e7751f92
Jef King <jef@agilebusinesscloud.com>
2015-01-16 07:16:42 -0800

    more coverage

---
+++ King.MQC.Unit.Test/GlobalConfigurationTests.cs
@@ -0,0 +1,15 @@
+﻿namespace King.MQC.Unit.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class GlobalConfigurationTests
+    {
+    }
+}
--- King.MQC.Unit.Test/RouteAttributeTests.cs
+++ King.MQC.Unit.Test/RouteAttributeTests.cs
@@ -1,9 +1,32 @@
 ﻿namespace King.MQC.Unit.Test
 {
     using NUnit.Framework;
+    using System;
 
     [TestFixture]
     public class RouteAttributeTests
     {
+        [Test]
+        public void Constructor()
+        {
+            new RouteAttribute(Guid.NewGuid().ToString());
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorNameNull()
+        {
+            new RouteAttribute(null);
+        }
+
+        [Test]
+        public void Name()
+        {
+            var expected = Guid.NewGuid().ToString();
+            
+            var ra = new RouteAttribute(expected);
+
+            Assert.AreEqual(expected, ra.Name);
+        }
     }
 }

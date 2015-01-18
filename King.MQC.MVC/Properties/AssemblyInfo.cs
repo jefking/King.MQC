82a5e55e3c0a3aa52a849668c933d328109a3757
Jef King <jef@agilebusinesscloud.com>
2015-01-18 03:25:15 -0800

    MVC Demo

---
+++ King.MQC.MVC/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("King.MQC.MVC")]
+[assembly: AssemblyDescription("MVC using MQC")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("King.MQC.MVC")]
+[assembly: AssemblyCopyright("Copyright © Jef King 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4202fcd7-f474-4230-b09c-e38a8d603b06")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ King.MQC.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(King.MQC.MVC.Startup))]
+namespace King.MQC.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

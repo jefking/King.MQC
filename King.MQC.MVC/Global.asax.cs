ce4d27cb18e9e4c7e18319bf3bea26a0e36c4eb6
Jef King <jef@agilebusinesscloud.com>
2015-01-18 03:29:23 -0800

    fix namespaces

--- King.MQC.MVC/Global.asax.cs
+++ King.MQC.MVC/Global.asax.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
-using System.Web.Routing;
 
 namespace King.MQC.MVC
 {
@@ -14,9 +8,9 @@ namespace King.MQC.MVC
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+            System.Web.Http.GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            RouteConfig.RegisterRoutes(System.Web.Routing.RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }

61f9c59c4e1ee1f3f5e75179f0537dde957d6422
Jef King <jefkin@microsoft.com>
2015-01-24 07:12:49 -0800

    Routing Model

---
+++ King.MQC/Routing/RoutedData.cs
@@ -0,0 +1,25 @@
+﻿namespace King.MQC.Routing
+{
+    public class RoutedData<T>
+    {
+        #region Properties
+        /// <summary>
+        /// Route
+        /// </summary>
+        public virtual string Route
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Model
+        /// </summary>
+        public virtual T Model
+        {
+            get;
+            set;
+        }
+        #endregion
+    }
+}
